bf3ad87de5b16bda733230aed2f71eef3b29ac2d
Mohammad Hj Hassan <35444902+MohammadHHassan@users.noreply.github.com>
2024-01-22 15:16:11 +0800

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+package day1;
+
+public class HelloWorld 
+{
+	public static void main(String[] agrs) 		// Main method
+	{
+		System.out.print("Hello World!\nHello World!\nHello World!");	// Print Hello World! three times in a new line
+		System.out.print("Hello World!\nHello World!\nHello World!");
+	}
+}
